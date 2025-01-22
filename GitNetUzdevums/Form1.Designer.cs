069f39e82b85c69d3e323e6bcf52883e5280319c
Leo Lubans <leo.lubans@outlook.com>
2025-01-22 12:19:46 +0200

    Added a group box and a button

--- GitNetUzdevums/Form1.Designer.cs
+++ GitNetUzdevums/Form1.Designer.cs
@@ -32,23 +32,28 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.GroupBoxLeoLubans = new System.Windows.Forms.GroupBox();
+            this.ButtonLLubans = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
+            this.GroupBoxLeoLubans.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(594, 21);
+            this.pictureBox1.Location = new System.Drawing.Point(891, 32);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(397, 368);
+            this.pictureBox1.Size = new System.Drawing.Size(596, 566);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(119, 19);
+            this.button1.Location = new System.Drawing.Point(178, 29);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 35);
             this.button1.TabIndex = 1;
             this.button1.Text = "y=sinx";
             this.button1.UseVisualStyleBackColor = true;
@@ -56,9 +61,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(6, 19);
+            this.button2.Location = new System.Drawing.Point(9, 29);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(112, 35);
             this.button2.TabIndex = 2;
             this.button2.Text = "y=x*x";
             this.button2.UseVisualStyleBackColor = true;
@@ -68,24 +74,49 @@
             // 
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Location = new System.Drawing.Point(12, 21);
+            this.groupBox1.Location = new System.Drawing.Point(18, 32);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 53);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(300, 82);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Andrejs Liepiņš";
             // 
+            // GroupBoxLeoLubans
+            // 
+            this.GroupBoxLeoLubans.Controls.Add(this.ButtonLLubans);
+            this.GroupBoxLeoLubans.Location = new System.Drawing.Point(27, 194);
+            this.GroupBoxLeoLubans.Name = "GroupBoxLeoLubans";
+            this.GroupBoxLeoLubans.Size = new System.Drawing.Size(281, 236);
+            this.GroupBoxLeoLubans.TabIndex = 4;
+            this.GroupBoxLeoLubans.TabStop = false;
+            this.GroupBoxLeoLubans.Text = "Leo Lubans";
+            // 
+            // ButtonLLubans
+            // 
+            this.ButtonLLubans.Location = new System.Drawing.Point(7, 53);
+            this.ButtonLLubans.Name = "ButtonLLubans";
+            this.ButtonLLubans.Size = new System.Drawing.Size(90, 58);
+            this.ButtonLLubans.TabIndex = 0;
+            this.ButtonLLubans.Text = "y=x";
+            this.ButtonLLubans.UseVisualStyleBackColor = true;
+            this.ButtonLLubans.Click += new System.EventHandler(this.ButtonLLubans_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1016, 415);
+            this.ClientSize = new System.Drawing.Size(1524, 638);
+            this.Controls.Add(this.GroupBoxLeoLubans);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Funkciju grafiki";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
+            this.GroupBoxLeoLubans.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -96,6 +127,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox GroupBoxLeoLubans;
+        private System.Windows.Forms.Button ButtonLLubans;
     }
 }
 
